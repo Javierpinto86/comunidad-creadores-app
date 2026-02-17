40dc9324ebf92bf958dd14c31b025931ef923760
Javierpinto86 <javierpintobanuls@gmail.com>
2026-02-17 13:19:31 +0100

    Align library template name with fase 1 spec

--- library/views.py
+++ library/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def library_home_view(request):
-    return render(request, 'library/library_home.html')
+    return render(request, 'library/biblioteca.html')
